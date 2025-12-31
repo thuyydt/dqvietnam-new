aee274a09487ba15ec9052b43a29331b3221754c
phinq <phinq@omt-quality.jp>
2025-12-31 09:58:08 +0700

    refactor: improve SEO

--- apps/dqvietnam/application/views/public/_block/menu.php
+++ apps/dqvietnam/application/views/public/_block/menu.php
@@ -1,10 +1,14 @@
 <nav class="navbar navbar-left s-nav" aria-label="Menu trái">
   <ul class="nav">
-    <li class="nav-item"><a href="<?= BASE_URL ?>" class="nav-link" title="Trang Chủ">Trang Chủ</a></li>
+    <li class="nav-item">
+      <a href="<?= BASE_URL ?>" class="nav-link" title="Trang Chủ">Trang Chủ</a>
+    </li>
     <?= $right ?>
   </ul>
 </nav>
-<a class="navbar-brand" href="<?= BASE_URL ?>" aria-label="Trang chủ"><img src="public/images/logo-01.webp" alt="DQ Vietnam Logo" decoding="async" /></a>
+<a class="navbar-brand" href="<?= BASE_URL ?>" aria-label="Trang chủ">
+  <img src="public/images/logo-01.webp" alt="DQ Vietnam Logo" decoding="async" fetchpriority="high" />
+</a>
 <nav class="navbar navbar-right" aria-label="Menu phải">
   <ul class="nav">
     <?= $left ?>
